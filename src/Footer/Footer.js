a31e8208c6717b0bdf0a18bcb30d68b53abedd5b
skolli1510 <skolli8@charlotte.edu>
2024-09-26 22:29:31 +0530

    Brought personal Budget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bottom">
+        <div className="center">
+            Changes done by Sushma Kolli.
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
